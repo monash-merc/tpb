cdd9393f55edfb9519a58de62493cd6eb1e75c6c
xiyu <xiaoming.yu@monash.edu>
2020-10-30 01:18:30 +1100

    added xss interceptor

---
+++ src/java/edu/monash/merc/struts2/intercepter/XssInterceptor.java
@@ -0,0 +1,77 @@
+/*
+ * Copyright (c) 2011-2013, Monash e-Research Centre
+ * (Monash University, Australia)
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 	* Redistributions of source code must retain the above copyright
+ * 	  notice, this list of conditions and the following disclaimer.
+ * 	* Redistributions in binary form must reproduce the above copyright
+ * 	  notice, this list of conditions and the following disclaimer in the
+ * 	  documentation and/or other materials provided with the distribution.
+ * 	* Neither the name of the Monash University nor the names of its
+ * 	  contributors may be used to endorse or promote products derived from
+ * 	  this software without specific prior written permission.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public License for more details.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ * You should have received a copy of the GNU Affero General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package edu.monash.merc.struts2.intercepter;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
+import org.apache.commons.lang3.StringEscapeUtils;
+import org.apache.log4j.Logger;
+
+import java.util.Map;
+
+/**
+ * XssInterceptor class extends AbstractInterceptor which provides the Interceptor of request for avoid Xss attack
+ * <p>
+ * https://www.javacodegeeks.com/2012/07/anti-cross-site-scripting-xss-filter.html
+ *
+ * @author Simon Yu
+ * <p/>
+ * Email: xiaoming.yu@monash.edu
+ * @version 1.0
+ * @since 1.0
+ * <p/>
+ * Date: 21/12/11 1:27 PM
+ */
+public class XssInterceptor extends AbstractInterceptor {
+
+    private Logger logger = Logger.getLogger(this.getClass().getName());
+
+    @Override
+    public String intercept(ActionInvocation invocation) throws Exception {
+        ActionContext actionContext = invocation.getInvocationContext();
+        Map<String, Object> map = actionContext.getParameters();
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            String value = ((String[]) (entry.getValue()))[0];
+            entry.setValue(StringEscapeUtils.escapeHtml4(value));
+            if (logger.isDebugEnabled()) {
+                logger.debug("parameter value " + value);
+            }
+        }
+        return invocation.invoke();
+    }
+
+}
